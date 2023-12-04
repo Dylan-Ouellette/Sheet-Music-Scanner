4329651c0d12228a1f801c5d2a846b3d4c930242
CalvinN13470 <77709342+CalvinN13470@users.noreply.github.com>
2023-12-04 11:30:55 -0500

    added classifications and creating barboxes

---
+++ src/Treble_Classification.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+from Classification_Functions import  checkDistance
+
+def Treble_Classifcation(img_gray, img_rgb, template_file, threshold):
+
+    template = cv.imread(template_file, cv.IMREAD_GRAYSCALE)
+    treble_w, treble_h = template.shape[::-1]
+    res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+    loc = np.where( res >= threshold)
+    treble_pt_list = []
+
+    #draws rectangles
+    for pt in zip(*loc[::-1]):
+        if checkDistance(treble_pt_list, pt, 50):
+            treble_pt_list.append(pt)
+            cv.rectangle(img_rgb, pt, (pt[0] + treble_w, pt[1] + treble_h), (0,255,0), 2)
+
+    print(len(treble_pt_list))
+
+    page_length, page_width = img_gray.shape
+
+    #removes top part of page above the first treble clef to remove classification of title
+    for y in range(page_length):
+            for x in range(page_width):
+                # Check if the pixel is above the specified y-coordinate
+                if y < treble_pt_list[0][1] - 30:
+                    # Set pixel color to white (255, 255, 255)
+                    img_gray[y, x] = 255
+    
+    treble_list = []
+    for treble_pt in treble_pt_list:
+         treble_list.append([treble_pt, "treble"])
+
+    return treble_list
